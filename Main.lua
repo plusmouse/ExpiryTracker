b593edd986f265d2e701a9c21614642bd94c059b
plusmouse <plusmouse@protonmail.com>
2024-12-21 18:47:01 +0000

    Improvements

--- Main.lua
+++ Main.lua
@@ -1,5 +1,6 @@
 EventUtil.ContinueOnAddOnLoaded("ExpiryTracker", function()
   local frame = CreateFrame("Frame", nil, UIParent, "ButtonFrameTemplate")
+  frame:Hide()
   ButtonFrameTemplate_HidePortrait(frame)
   ButtonFrameTemplate_HideButtonBar(frame)
   frame.Inset:Hide()
@@ -9,24 +10,6 @@ EventUtil.ContinueOnAddOnLoaded("ExpiryTracker", function()
   frame:SetUserPlaced(false)
 
   frame:SetSize(800, 500)
-
-  local all = {}
-
-  local currentTime = time()
-  for _, name in ipairs(Syndicator.API.GetAllCharacters()) do
-    local c = Syndicator.API.GetCharacter(name)
-    local auctions = CopyTable(c.auctions)
-    for _, a in ipairs(auctions) do
-      a.character = c.details.character
-      a.realm = c.details.realm
-      a.expirationTime = a.expirationTime or 0
-    end
-    tAppendAll(all, auctions)
-  end
-
-  table.sort(all, function(a, b)
-    return a.expirationTime < b.expirationTime
-  end)
 
   local scrollBox = CreateFrame("Frame", nil, frame, "WowScrollBoxList")
   local scrollBar = CreateFrame("EventFrame", nil, frame, "MinimalScrollBar")
@@ -55,7 +38,7 @@ EventUtil.ContinueOnAddOnLoaded("ExpiryTracker", function()
       f.expiry:SetPoint("RIGHT")
       f.expiry:SetJustifyH("RIGHT")
     end
-    f.link:SetText(data.itemLink)
+    f.link:SetText(data.itemLink .. " " .. LIGHTBLUE_FONT_COLOR:WrapTextInColorCode("x" .. data.itemCount))
     f.realm:SetText(data.realm)
     f.character:SetText(data.character)
     local text = GRAY_FONT_COLOR:WrapTextInColorCode("Unknown")
@@ -77,10 +60,35 @@ EventUtil.ContinueOnAddOnLoaded("ExpiryTracker", function()
   scrollBar:SetPoint("TOPRIGHT", -10, -50)
   scrollBar:SetPoint("BOTTOMRIGHT", -10, 20)
 
-  scrollBox:SetDataProvider(CreateDataProvider(all))
 
-  frame:Show()
   frame:SetTitle("Auction Expiration Times")
   frame:EnableMouse(true)
   frame:SetPoint("CENTER")
+  frame:SetScript("OnShow", function()
+    local all = {}
+
+    local currentTime = time()
+    for _, name in ipairs(Syndicator.API.GetAllCharacters()) do
+      local c = Syndicator.API.GetCharacter(name)
+      local auctions = CopyTable(c.auctions)
+      for _, a in ipairs(auctions) do
+        a.character = c.details.character
+        a.realm = c.details.realm
+        a.expirationTime = a.expirationTime or 0
+      end
+      tAppendAll(all, auctions)
+    end
+
+    table.sort(all, function(a, b)
+      return a.expirationTime < b.expirationTime
+    end)
+
+    scrollBox:SetDataProvider(CreateDataProvider(all))
+  end)
+
+  SlashCmdList["ExpiryTracker"] = function()
+    frame:Show()
+  end
+  SLASH_ExpiryTracker1 = "/etr"
+  SLASH_ExpiryTracker2 = "/etr"
 end)
